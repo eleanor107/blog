64117d0d49c4d619c9f7c83b4996af85cad8ad9c
Eleanor Lee <eleanorlee@dyn-129-236-212-164.dyn.columbia.edu>
2016-03-31 22:40:24 -0400

    Add migration file

--- posts/models.py
+++ posts/models.py
@@ -27,7 +27,7 @@ class Post(models.Model):
 		blank=True, 
 		width_field = "width_field", 
 		height_field = "height_field")
-	image_path = models.TextField()
+	image_path = models.TextField(default="")
 	height_field = models.IntegerField(default = 0)
 	width_field = models.IntegerField(default = 0)
 	content = models.TextField()
