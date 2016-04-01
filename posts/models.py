8f4294d105386e5de69a14f993148803af355fea
Eleanor Lee <eleanorlee@dyn-129-236-212-164.dyn.columbia.edu>
2016-03-31 22:38:55 -0400

    Fix Image Problem

--- posts/models.py
+++ posts/models.py
@@ -27,6 +27,7 @@ class Post(models.Model):
 		blank=True, 
 		width_field = "width_field", 
 		height_field = "height_field")
+	image_path = models.TextField()
 	height_field = models.IntegerField(default = 0)
 	width_field = models.IntegerField(default = 0)
 	content = models.TextField()
